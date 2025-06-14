d685624970b897985c2b0b3710c022549fdf6cba
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-14 22:52:58 +0500

    Task List Added

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='flex justify-flex-start items-center flex-nowrap gap-5 h-[300px] w-[100%] mx-2 py-4  mt-10 overflow-x-auto'>
+        <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+        
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+
+         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
+            <h1>TaskList</h1>
+        </div>
+    </div>
+  )
+}
+
+export default TaskList
