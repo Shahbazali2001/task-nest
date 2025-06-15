dd5c6b7af0f0ff3dcb2dd728c211ce7290b38878
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-16 00:04:22 +0500

    Task Form

---
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <div>
+        <form>
+            <h3>Task Title</h3>
+            <input type="text" placeholder='Enter your name'/>
+            
+        </form>
+    </div>
+  )
+}
+
+export default AdminDashboard
