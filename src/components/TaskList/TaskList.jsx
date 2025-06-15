b7e5e2e1663bda4cc2ff10cf6f09e090b59d88fb
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-15 23:05:33 +0500

    Task Item Changes

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,41 +3,19 @@ import React from 'react'
 const TaskList = () => {
   return (
     <div id='tasklist' className='flex justify-flex-start items-center flex-nowrap gap-5 h-[300px] w-[100%] mx-2 py-4  mt-10 overflow-x-auto'>
-        <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
+        <div className='flex-shrink-0 h-full  w-[300px] bg-red-400 rounded-xl'>
+            <div className='flex flex-row justify-between items-center p-5'>
+                <h3 className='bg-red-600 p-1 rounded-md'>
+                    High
+                </h3>
+
+                <h4 className=''>
+                    15-June-2025
+                </h4>
+            </div>
         </div>
 
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
-
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
-
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
         
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
-
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
-
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
-
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
-
-         <div className='flex-shrink-0 h-full  w-[300px] bg-blue-500 rounded-xl'>
-            <h1>TaskList</h1>
-        </div>
     </div>
   )
 }
