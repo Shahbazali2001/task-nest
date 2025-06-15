34104657cf437063152a2b136ba1601ba8ac7ab4
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-15 23:47:23 +0500

    Task Items Created

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,19 +3,150 @@ import React from 'react'
 const TaskList = () => {
   return (
     <div id='tasklist' className='flex justify-flex-start items-center flex-nowrap gap-5 h-[300px] w-[100%] mx-2 py-4  mt-10 overflow-x-auto'>
-        <div className='flex-shrink-0 h-full  w-[300px] bg-red-400 rounded-xl'>
-            <div className='flex flex-row justify-between items-center p-5'>
-                <h3 className='bg-red-600 p-1 rounded-md'>
+        
+        <div className='flex-shrink-0 h-full  w-[300px] bg-red-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
                     High
                 </h3>
 
-                <h4 className=''>
-                    15-June-2025
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
                 </h4>
             </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
 
-        
+        <div className='flex-shrink-0 h-full  w-[300px] bg-green-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
+                    High
+                </h3>
+
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
+                </h4>
+            </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className='flex-shrink-0 h-full  w-[300px] bg-blue-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
+                    High
+                </h3>
+
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
+                </h4>
+            </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className='flex-shrink-0 h-full  w-[300px] bg-yellow-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
+                    High
+                </h3>
+
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
+                </h4>
+            </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className='flex-shrink-0 h-full  w-[300px] bg-pink-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
+                    High
+                </h3>
+
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
+                </h4>
+            </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className='flex-shrink-0 h-full  w-[300px] bg-orange-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
+                    High
+                </h3>
+
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
+                </h4>
+            </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className='flex-shrink-0 h-full  w-[300px] bg-gray-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
+                    High
+                </h3>
+
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
+                </h4>
+            </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className='flex-shrink-0 h-full  w-[300px] bg-green-400 p-5 rounded-xl'>
+            <div className='flex flex-row justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded-md font-bold'>
+                    High
+                </h3>
+
+                <h4 className='text-sm font-bold'>
+                    15 June 2025
+                </h4>
+            </div>
+
+            <h2 className=' mt-5 text-2xl font-semibold'>
+                Make a YouTube Video
+            </h2>
+
+            <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
     </div>
   )
 }
