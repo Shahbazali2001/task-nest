20b37dcd2619abccf09e48aa4c4c74ffc01851a8
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-17 01:02:32 +0500

    Admin Dashboard Form Created

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 
 const AdminDashboard = () => {
   return (
-    <div>
-        <form>
+    <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-2xl font-bold'>Admin Dashboard</h1>
+        <form action="" className='flex flex-col gap-5 p-5'>
             <h3>Task Title</h3>
-            <input type="text" placeholder='Enter your name'/>
+            <input type="text" placeholder='Enter task title'/>
             <h3>Task Description</h3>
-            <input type="text" placeholder='Enter your name'/>
-            <h3>Task Status</h3>
-            <input type="text" placeholder='Enter your name'/>
+            <textarea placeholder='Enter task description' cols={10} rows={5}></textarea>
+            <h3>Date</h3>
+            <input type="date"/>
+            <h3>Assign To</h3>
+            <input type="text" placeholder='Enter assignee name'/>
+            <h3>Category</h3>
+            <input type="text" placeholder='Design, Development or Testing etc.'/>
+            <button>Create Task</button>
             
         </form>
     </div>
